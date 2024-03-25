43a2e693e24e9d79f7044195a1be542e3b53080b
pun-intended <31040416+pun-intended@users.noreply.github.com>
2024-03-25 20:45:39 +0900

    Update library-schema.sql

--- capstone2/library-backend/library-schema.sql
+++ capstone2/library-backend/library-schema.sql
@@ -1,13 +1,14 @@
 CREATE TABLE schools (
     id SERIAL PRIMARY KEY,
     name VARCHAR(25) NOT NULL,
-    address TEXT NOT NULL
+    address TEXT
 );
 
 CREATE TABLE classes (
     id SERIAL PRIMARY KEY,
     name VARCHAR(20) NOT NULL,
-    school_id INTEGER NOT NULL
+    school_id INTEGER NOT NULL 
+        FOREIGN KEY REFERENCES schools(id) ON DELETE CASCADE
 );
 
 CREATE TABLE users (
@@ -15,12 +16,12 @@ CREATE TABLE users (
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
     class_id INTEGER
-        REFERENCES classes ON DELETE SET NULL,
+        FOREIGN KEY REFERENCES classes(id) ON DELETE SET NULL,
     school_id INTEGER
-        REFERENCES schools ON DELETE SET NULL,
+        FOREIGN KEY REFERENCES schools(id) ON DELETE SET NULL,
     password TEXT NOT NULL,
-    role string NOT NULL DEFAULT "user",
-        CHECK (role IN ("user", "school_admin", "master"))
+    role VARCHAR(14) NOT NULL DEFAULT "user",
+        CHECK (role IN ("user", "school_admin", "master_admin"))
 );
 
 CREATE TABLE students (
@@ -28,37 +29,42 @@ CREATE TABLE students (
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
     school_id INTEGER
-        REFERENCES schools ON DELETE SET NULL,
+        FOREIGN KEY REFERENCES schools(id) ON DELETE SET NULL,
     class_id INTEGER
-        REFERENCES classes ON DELETE SET NULL,
+        FOREIGN KEY REFERENCES classes(id) ON DELETE SET NULL,
     level TEXT NOT NULL
 );
 
-CREATE TABLE books (
-    id SERIAL PRIMARY KEY,
-    isbn TEXT NOT NULL,
+CREATE TABLE master_books (
+    isbn text PRIMARY KEY,
     title TEXT NOT NULL,
     stage INTEGER NOT NULL,
-    set INTEGER,
-    school_id INTEGER,
-    condition VARCHAR(10) NOT NULL
-);
+)
 
 CREATE TABLE book_sets (
     set_id SERIAL PRIMARY KEY,
-    school_id INTEGER REFERENCES schools,
-
+    school_id INTEGER 
+        FOREIGN KEY REFERENCES schools(id) ON DELETE SET NULL,
 )
+
+CREATE TABLE books (
+    id SERIAL PRIMARY KEY,
+    isbn TEXT NOT NULL
+        FOREIGN KEY REFERENCES master_books(isbn) ON DELETE CASCADE,
+    set INTEGER
+        FOREIGN KEY REFERENCES book_sets(set_id) ON DELETE SET 0,
+    school_id INTEGER
+        FOREIGN KEY REFERENCES schools(id) ON DELETE SET NULL,
+    condition VARCHAR(10) NOT NULL
+);
 
 -- // TODO - fix dateTime formatting
 CREATE TABLE borrow_record (
     id SERIAL PRIMARY KEY, 
     student_id INTEGER NOT NULL
-        REFERENCES students ON DELETE CASCADE, 
-    book_id INTEGER NOT NULL
-        REFERENCES books ON DELETE CASCADE,
-        -- This will be a problem for record keeping. How to replace with generic code?
-        -- TRIGGER? MasterBooks table? Normalization could complicate function calls
+        FOREIGN KEY REFERENCES students(id) ON DELETE CASCADE, 
+    book_isbn INTEGER NOT NULL
+        FOREIGN KEY REFERENCES master_books(isbn) ON DELETE CASCADE,
     borrow_date VARCHAR(12) NOT NULL,
     return_date VARCHAR(12)
 );
