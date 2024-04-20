b304111e6e4cea294e52e29d7a6ca9912b705dfd
pun-intended <31040416+pun-intended@users.noreply.github.com>
2024-04-20 19:04:04 +0900

    Correct errors in schema
    Correct errors in schema
    Adapt schema to represent new structure of site

--- capstone2/library-backend/library-schema.sql
+++ capstone2/library-backend/library-schema.sql
@@ -1,14 +1,20 @@
 CREATE TABLE schools (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(25) NOT NULL,
-    address TEXT
+    name VARCHAR(25) NOT NULL
 );
 
 CREATE TABLE classes (
     id SERIAL PRIMARY KEY,
     name VARCHAR(20) NOT NULL,
     school_id INTEGER NOT NULL 
-        FOREIGN KEY REFERENCES schools(id) ON DELETE CASCADE
+        REFERENCES schools(id) ON DELETE CASCADE,
+    level VARCHAR(5) NOT NULL
+);
+
+CREATE TABLE master_books (
+    isbn text PRIMARY KEY,
+    title TEXT NOT NULL,
+    stage INTEGER NOT NULL
 );
 
 CREATE TABLE users (
@@ -16,12 +22,12 @@ CREATE TABLE users (
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
     class_id INTEGER
-        FOREIGN KEY REFERENCES classes(id) ON DELETE SET NULL,
+        REFERENCES classes(id) ON DELETE SET NULL,
     school_id INTEGER
-        FOREIGN KEY REFERENCES schools(id) ON DELETE SET NULL,
+        REFERENCES schools(id) ON DELETE SET NULL,
     password TEXT NOT NULL,
-    role VARCHAR(14) NOT NULL DEFAULT "user",
-        CHECK (role IN ("user", "school_admin", "master_admin"))
+    role VARCHAR(14) NOT NULL DEFAULT 'user',
+        CHECK (role IN ('user', 'school_admin', 'master_admin'))
 );
 
 CREATE TABLE students (
@@ -29,37 +35,31 @@ CREATE TABLE students (
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
     class_id INTEGER
-        FOREIGN KEY REFERENCES classes(id) ON DELETE SET NULL
-);
-
-CREATE TABLE master_books (
-    isbn text PRIMARY KEY,
-    title TEXT NOT NULL,
-    stage INTEGER NOT NULL,
+        REFERENCES classes(id) ON DELETE SET NULL
 );
 
 CREATE TABLE book_sets (
     set_id SERIAL PRIMARY KEY,
     school_id INTEGER 
-        FOREIGN KEY REFERENCES schools(id) ON DELETE SET NULL,
+        REFERENCES schools(id) ON DELETE SET NULL
 );
 
 CREATE TABLE books (
     id SERIAL PRIMARY KEY,
     isbn TEXT NOT NULL
-        FOREIGN KEY REFERENCES master_books(isbn) ON DELETE CASCADE,
+        REFERENCES master_books(isbn) ON DELETE CASCADE,
     book_set INTEGER
-        FOREIGN KEY REFERENCES book_sets(set_id) ON DELETE SET 0,
-    condition VARCHAR(10) NOT NULL
+        REFERENCES book_sets(set_id) ON DELETE SET NULL,
+    condition VARCHAR(10) DEFAULT 'Great'
 );
 
 -- // TODO - fix dateTime formatting
 CREATE TABLE borrow_record (
     id SERIAL PRIMARY KEY, 
     student_id INTEGER NOT NULL
-        FOREIGN KEY REFERENCES students(id) ON DELETE CASCADE, 
-    book_isbn INTEGER NOT NULL
-        FOREIGN KEY REFERENCES master_books(isbn) ON DELETE CASCADE,
+        REFERENCES students(id) ON DELETE CASCADE, 
+    book_isbn TEXT NOT NULL
+        REFERENCES master_books(isbn) ON DELETE CASCADE,
     borrow_date VARCHAR(12) NOT NULL,
     return_date VARCHAR(12)
 );
