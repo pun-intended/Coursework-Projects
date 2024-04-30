27fa6e0ef62513a9ef466d2dc8801133183442ce
pun-intended <31040416+pun-intended@users.noreply.github.com>
2024-04-30 16:24:34 +0900

    Update _testCommon.js
    Add schools, stage 2 books to test data

--- capstone2/library-backend/models/_testCommon.js
+++ capstone2/library-backend/models/_testCommon.js
@@ -37,6 +37,14 @@ async function commonBeforeAll() {
     );
 
     await db.query(`
+    INSERT INTO schools(id, name)
+    VALUES
+      (101, 'school A'),
+      (102, 'school B'),
+      (103, 'school C')`
+    );
+
+    await db.query(`
     INSERT INTO classes (id, school_id, name)
     VALUES
       (1001, 101, 'Class A'),
@@ -47,7 +55,8 @@ async function commonBeforeAll() {
       (1006, 102, 'Class C'),
       (1007, 103, 'Class A'),
       (1008, 103, 'Class B'),
-      (1009, 103, 'Class C')`);
+      (1009, 103, 'Class C')`
+    );
 
     await db.query(
     `INSERT INTO students (id, first_name, last_name, level)
@@ -62,31 +71,31 @@ async function commonBeforeAll() {
       (1008, 'Bendick', 'Knightley', 1002),
       (1009, 'Goldi', 'Fairbrass', 1006),
       (1010, 'Nealy', 'Simonsen', 1004)`
-    )
+    );
 
     await db.query(
-      `INSERT INTO master_books (isbn, title, stage)
-      VALUES
-        ('014130670X', 'Turtle and Snake Go Camping', 1),
-        ('448457636', 'Bake, Mice, Bake!', 1),
-        ('448461579', 'We Are Twins', 1),
-        ('448461587', 'Max Has a Fish', 1),
-        ('448463059', 'Cat Days', 1),
-        ('448462648', 'Clara and Clem Take a Ride', 1),
-        ('9780448496467', 'Guppy Up!', 1),
-        ('448463768', 'On a Farm', 1),
-        ('0448461803', 'A New Friend', 1),
-        ('448464713', 'At the Beach', 1),
-        ('448467194', 'In the Forest', 1)`
-    )
+    `INSERT INTO master_books (isbn, title, stage)
+    VALUES
+      ('014130670X', 'Turtle and Snake Go Camping', 1),
+      ('448457636', 'Bake, Mice, Bake!', 1),
+      ('448461579', 'We Are Twins', 1),
+      ('448461587', 'Max Has a Fish', 1),
+      ('448463059', 'Cat Days', 1),
+      ('448462648', 'Clara and Clem Take a Ride', 1),
+      ('9780448496467', 'Guppy Up!', 1),
+      ('448463768', 'On a Farm', 2),
+      ('0448461803', 'A New Friend', 2),
+      ('448464713', 'At the Beach', 2),
+      ('448467194', 'In the Forest', 2)`
+    );
     
     await db.query(
-      `INSERT INTO book_sets (set_id, school_id)
-      VALUES
-        ('1', '101'),
-        ('2', '101'),
-        ('3', '102')`
-    )
+    `INSERT INTO book_sets (set_id, school_id)
+    VALUES
+      ('1', '101'),
+      ('2', '101'),
+      ('3', '102')`
+    );
 
     await db.query(
     `INSERT INTO books (id, isbn, set_id)
@@ -102,7 +111,7 @@ async function commonBeforeAll() {
       ('109', '0448461803', '1'),
       ('110', '448464713', '1'),
       ('112', '448467194', '1'),
-      ('121', '014130670X', '1'),
+      ('121', '014130670X', '2'),
       ('122', '448457636', '2'),
       ('123', '448461579', '2'),
       ('124', '448461587', '2'),
@@ -123,21 +132,22 @@ async function commonBeforeAll() {
       ('148', '448463768', '3'),
       ('149', '0448461803', '3'),
       ('150', '448464713', '3'),
-      ('152', '448467194', '3')
-    `)
+      ('152', '448467194', '3')`
+    );
 
     await db.query(`
     INSERT INTO borrow_record (student_id, book_id, borrow_date, return_date)
     VALUES 
-    ('1001', '102', '2023-10-10', '2023-10-17'),
-    ('1001', '103', '2023-10-17', '2023-10-24'),
-    ('1001', '104', '2023-10-24', NULL),
-    ('1003', '103', '2023-10-27', NULL),
-    ('1005', '102', '2023-10-19', NULL),
-    ('1007', '124', '2023-10-19', NULL),
-    ('1004', '111', '2023-10-18', NULL),
-    ('1006', '102', '2023-10-17', NULL)`)
-    }
+      ('1001', '102', '2023-10-10', '2023-10-17'),
+      ('1001', '103', '2023-10-17', '2023-10-24'),
+      ('1001', '104', '2023-10-24', NULL),
+      ('1003', '103', '2023-10-27', NULL),
+      ('1005', '102', '2023-10-19', NULL),
+      ('1007', '124', '2023-10-19', NULL),
+      ('1004', '111', '2023-10-18', NULL),
+      ('1006', '102', '2023-10-17', NULL)`
+    );
+}
     
 
 async function commonBeforeEach() {
