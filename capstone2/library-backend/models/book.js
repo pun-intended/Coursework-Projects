9b6c78968f27ccf752f12835123dcde01bd83dbe
pun-intended <31040416+pun-intended@users.noreply.github.com>
2024-05-09 21:16:21 +0900

    Update book.js
    Create method to return all copies of a book

--- capstone2/library-backend/models/book.js
+++ capstone2/library-backend/models/book.js
@@ -84,6 +84,52 @@ class Book {
     }
 
     /**
+     * Getbook IDs for all copies of a book
+     * 
+     * {isbn, school_id (optional)} => {book_id, title, stage, condition, student}
+     *  where student is {student_id, first_name, last_name, class_id, borrowDate}
+     * 
+     */
+    static async getCopies(isbn, school_id = null){
+        const baseQuery = `
+            SELECT  B.id as book_id,
+                    B.isbn,
+                    M.title,
+                    M.stage,
+                    B.condition,
+                    S.id AS student_id,
+                    S.first_name,
+                    S.last_name,
+                    S.class_id,
+                    S.borrow_date,
+                    set.school_id
+            FROM books B
+            JOIN master_books M ON B.isbn = M.isbn
+            JOIN book_sets set ON B.set_id = set.set_id
+            FULL JOIN (SELECT     S.id,
+                            S.first_name,
+                            S.last_name,
+                            S.class_id,
+                            rec.borrow_date,
+                            rec.book_id
+                    FROM borrow_record rec
+                    JOIN students S ON S.id = rec.student_id
+                    JOIN books B ON B.id = rec.book_id
+                    WHERE rec.return_date IS NULL) AS S ON S.book_id = B.id
+            WHERE B.isbn = $1`
+
+        let res;
+
+        if(school_id){
+            res = await db.query(baseQuery + ` AND set.school_id = $2`, [isbn, school_id])
+        } else {
+            res = await db.query(baseQuery, [isbn])
+        }
+
+        return res.rows;
+    }
+
+    /**
      * Get all book data given the ID
      * 
      * {book_id} => {book_id, isbn, title, stage, condition, student}
@@ -133,20 +179,30 @@ class Book {
      * 
      * Returns [{isbn, title, stage, available}, ...] 
      */
-    static async getAllBooks(school_id, stage = null){
+    static async getAllBooks(school_id = null, stage = null){
 
         // Will the available field cause false positives when a book is taken out a second time? Limit IDs to null return dates?
         let books;
-        const baseQuery = 
-            `SELECT isbn,
-            title,
-            stage,
-            isbn IN (SELECT B.isbn FROM books B
-                            FULL JOIN borrow_record rec ON rec.book_id = B.id
-                            JOIN book_sets set ON set.set_id = b.set_id
-                    WHERE (rec.return_date IS NOT NULL OR rec.borrow_date IS NULL) AND set.school_id = $1) AS available
-            FROM master_books`
-        
+        let baseQuery
+
+        if(school_id){
+            baseQuery = 
+                `SELECT isbn,
+                title,
+                stage,
+                isbn IN (SELECT B.isbn FROM books B
+                                FULL JOIN borrow_record rec ON rec.book_id = B.id
+                                JOIN book_sets set ON set.set_id = b.set_id
+                        WHERE (rec.return_date IS NOT NULL OR rec.borrow_date IS NULL) AND set.school_id = $1) AS available
+                FROM master_books`
+        } else {
+            baseQuery = 
+                `SELECT isbn,
+                title,
+                stage,
+                false AS available
+                FROM master_books`
+        }
         if(stage){
             baseQuery += ` WHERE stage = $2`
             books = await db.query(baseQuery, [school_id, stage])
